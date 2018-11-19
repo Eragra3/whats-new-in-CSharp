45216baf3c52b5d4b34270b2e90d2cfa9ccfec2f
Daniel Bider <bider.daniel@gmail.com>
2018-11-19 22:08:17 +0100

    Added inline out variables section

---
+++ csharp/OutVariables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OutVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string indexString = "42";
+
+            int index;
+            if (int.TryParse(indexString, out index))
+            {
+                int nextIndex = index + 1;
+
+                Console.WriteLine($"Next person index is {nextIndex}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid index");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
