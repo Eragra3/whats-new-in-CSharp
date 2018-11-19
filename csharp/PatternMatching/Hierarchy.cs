eafcf3c9689981a37a6d31a1ff414cbf360b2dd9
Daniel Bider <bider.daniel@gmail.com>
2018-11-20 00:11:37 +0100

    Added more pattern matching examples

---
+++ csharp/PatternMatching/Hierarchy.cs
@@ -0,0 +1,34 @@
+﻿namespace PatternMatching
+{
+    abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string Index { get; set; }
+    }
+
+    class Programmer : Person, IHasSightDefect
+    {
+        public SkillLevel CSharp { get; set; }
+        public SkillLevel Java { get; set; }
+
+        public (double leftEye, double rightEye) SightDefect { get; set; }
+    }
+
+    interface IHasSightDefect
+    {
+        (double leftEye, double rightEye) SightDefect { get; }
+    }
+
+    enum SkillLevel
+    {
+        None,
+        Basic,
+        Intermediate,
+        Expert
+    }
+}
---
+++ csharp/PatternMatching/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PatternMatching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var daniel = new Student
+            {
+                FirstName = "Daniel",
+                LastName = "Bider",
+                Index = "222111"
+            };
+
+            var artur = new Programmer
+            {
+                FirstName = "Artur",
+                LastName = "Solex",
+                CSharp = SkillLevel.Intermediate,
+                Java = SkillLevel.Basic,
+                SightDefect = (0, 0)
+            };
+
+            var stepien = new Programmer
+            {
+                FirstName = "Paweł",
+                LastName = "Żondzi",
+                CSharp = SkillLevel.None,
+                Java = SkillLevel.Expert,
+                SightDefect = (-10.75, +12.5)
+            };
+
+            Console.WriteLine("Old way:");
+            OldPrintInfo(daniel);
+            OldPrintInfo(artur);
+            OldPrintInfo(stepien);
+
+            Console.WriteLine("\nNew way:");
+            NewPrintInfo(daniel);
+            NewPrintInfo(artur);
+            NewPrintInfo(stepien);
+
+            Console.ReadKey();
+        }
+
+        static void OldPrintInfo(Person p)
+        {
+            Console.WriteLine($"FirstName: {p.FirstName}");
+            Console.WriteLine($"LastName: {p.LastName}");
+
+            if (p is Student)
+            {
+                var s = (Student) p;
+                Console.WriteLine($"Index: {s.Index}");
+            }
+            else if (p is Programmer)
+            {
+                var pr = (Programmer) p;
+                Console.WriteLine($"CSharp: {pr.CSharp}");
+                Console.WriteLine($"Java: {pr.Java}");
+            }
+
+            if (p is IHasSightDefect)
+            {
+                var hsd = (IHasSightDefect)p;
+                Console.WriteLine($"SightDefect: {hsd.SightDefect}");
+            }
+
+            Console.WriteLine("-----------------------------------");
+        }
+
+        static void NewPrintInfo(Person p)
+        {
+            Console.WriteLine($"FirstName: {p.FirstName}");
+            Console.WriteLine($"LastName: {p.LastName}");
+
+            if (p is Student s)
+            {
+                Console.WriteLine($"Index: {s.Index}");
+            }
+            else if (p is Programmer pr)
+            {
+                Console.WriteLine($"CSharp: {pr.CSharp}");
+                Console.WriteLine($"Java: {pr.Java}");
+            }
+
+            if (p is IHasSightDefect hsd)
+            {
+                Console.WriteLine($"SightDefect: {hsd.SightDefect}");
+            }
+
+            Console.WriteLine("-----------------------------------");
+        }
+
+        static void SafeCast()
+        {
+            object numbers = Enumerable.Range(-3, 10).ToArray();
+
+            foreach (var number in numbers as IEnumerable<int>)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
