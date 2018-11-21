70d044844803b79220c9ab6f7a30989e6be30261
Daniel Bider <Daniel.Bider@fenergo.com>
2018-11-21 19:13:49 +0100

    C# 8 added and some formating

---
+++ csharp/NewTypeConstraints/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NewTypeConstraints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void RequireEnum<T>(T param) where T: Enum
+        {
+            throw new NotImplementedException();
+        }
+
+        static void RequireDelegate<T>(T param) where T: Delegate
+        {
+            throw new NotImplementedException();
+        }
+
+        static void RequireUnmanaged<T>(T param) where T: unmanaged
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
